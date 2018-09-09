c83be3cfebfc1937321fad46f25164faddfa6459
pranshu <uhsnarpggupta@gmail.com>
2018-09-09 17:25:56 +0530

    adding POJOs. Closes #2

---
+++ src/main/java/com/uhsnarp/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.uhsnarp.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/uhsnarp/sfgpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package com.uhsnarp.sfgpetclinic.model;
+
+public class Vet extends Person {
+    
+}
