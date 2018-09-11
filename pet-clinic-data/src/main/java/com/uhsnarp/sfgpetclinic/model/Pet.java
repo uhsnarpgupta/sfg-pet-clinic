ecc301aa6b4457b95719072106e8c5287d4f1790
pranshu <uhsnarpggupta@gmail.com>
2018-09-11 23:05:48 +0530

    Adding base entity. Closes #10

--- pet-clinic-data/src/main/java/com/uhsnarp/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/uhsnarp/sfgpetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package com.uhsnarp.sfgpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet extends Person {
+public class Pet extends BaseEntity {
     private PetType petType;
     private Owner owner;
     private LocalDate birthDate;
