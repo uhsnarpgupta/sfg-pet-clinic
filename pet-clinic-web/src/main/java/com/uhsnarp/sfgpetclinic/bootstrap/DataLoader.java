475d460c255757d502f8dc0ee05bba8e7b3c5146
pranshu <uhsnarpggupta@gmail.com>
2018-11-18 13:06:03 +0530

    Added PetTypes to Bootstrap. Closes #32

--- pet-clinic-web/src/main/java/com/uhsnarp/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/uhsnarp/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.uhsnarp.sfgpetclinic.bootstrap;
 
 import com.uhsnarp.sfgpetclinic.model.Owner;
+import com.uhsnarp.sfgpetclinic.model.PetType;
 import com.uhsnarp.sfgpetclinic.model.Vet;
 import com.uhsnarp.sfgpetclinic.services.OwnerService;
+import com.uhsnarp.sfgpetclinic.services.PetTypeService;
 import com.uhsnarp.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,12 +14,14 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
     //Befor Spring 2 @Autowired was required on constructor as well
     //@Autowired
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     /* public DataLoader() {
@@ -28,6 +32,15 @@ public class DataLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
         Owner owner1 = new Owner();
         //owner1.setId(1L);
         owner1.setFirstName("Mary");
