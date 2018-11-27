cf336effb3af2b55b7391f52aa8d67ce3c0cfe33
SAPIENT\pgup83 <pgupta114@sapient.com>
2018-11-27 19:08:38 +0530

    Updated owner with JPA entity mappings. Closes #36

--- pet-clinic-data/src/main/java/com/uhsnarp/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/uhsnarp/sfgpetclinic/model/PetType.java
@@ -1,11 +1,17 @@
 package com.uhsnarp.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity{
 
+    @Column(name = "name")
     private String name;
 
     public String getName() {
-
         return name;
     }
 
