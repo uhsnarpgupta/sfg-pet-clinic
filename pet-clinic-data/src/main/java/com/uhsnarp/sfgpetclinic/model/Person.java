a56725d6d95c79b48aaf9ea29e70f9b2522d71cd
pranshu <uhsnarpggupta@gmail.com>
2018-12-09 14:47:24 +0530

    Added project lombok to the project. Closes #51

--- pet-clinic-data/src/main/java/com/uhsnarp/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/uhsnarp/sfgpetclinic/model/Person.java
@@ -1,32 +1,30 @@
 package com.uhsnarp.sfgpetclinic.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 import javax.validation.constraints.NotEmpty;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @MappedSuperclass
-public class Person extends BaseEntity{
-    @Column(name="first_name")
+public class Person extends BaseEntity {
+    @Column(name = "first_name")
     @NotEmpty
     private String firstName;
-
-    @Column(name="last_name")
+    @Column(name = "last_name")
     @NotEmpty
     private String lastName;
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
+    public Person(Long id, String firstName, String lastName) {
+        super(id);
         this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 }
